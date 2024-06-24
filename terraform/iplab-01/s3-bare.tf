54afbebf93d0ffb23d67e64b0e729003ebd3729c
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2024-06-24 01:21:22 -0700

    updated bucket name

--- terraform/iplab-01/s3-bare.tf
+++ terraform/iplab-01/s3-bare.tf
@@ -1,7 +1,7 @@
 # Terraform template to create a basic s3 bucket
 
 resource "aws_s3_bucket" "iplab" {
-  bucket = "ipbucket-1000"
+  bucket = "ipbucket-10000"
 
   tags = {
     Name        = "iplab"
